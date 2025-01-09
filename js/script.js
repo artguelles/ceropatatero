0a264035adbbab11d2c883581ef775b2fed66503
xiangsoto <xiangsoto.145@gmail.com>
2025-01-10 00:20:48 +0100

    prueba

--- js/script.js
+++ js/script.js
@@ -51,46 +51,41 @@ $(document).ready(function() {
     });
 
     // VALIDAR FORM
-    $('form').submit(function (event) {
-        event.preventDefault();
+    // $('form').submit(function (event) {
+    //     event.preventDefault();
 
-        let isValid = true;
-        const name = $('#exampleFormControlInput1[type="text"]').val().trim();
-        const email = $('#exampleFormControlInput1[type="email"]').val().trim();
-        const eventType = $('#exampleFormControlSelect1').val();
-        const description = $('#exampleFormControlTextarea1').val().trim();
+    //     let isValid = true;
+    //     const name = $('#exampleFormControlInput1[type="text"]').val().trim();
+    //     const email = $('#exampleFormControlInput1[type="email"]').val().trim();
+    //     const eventType = $('#exampleFormControlSelect1').val();
+    //     const description = $('#exampleFormControlTextarea1').val().trim();
 
-        // Validar nombre
-        if (name === '') {
-            alert("Por favor, ingresa tu nombre y apellidos.");
-            isValid = false;
-        }
+    //     if (name === '') {
+    //         alert("Por favor, ingresa tu nombre y apellidos.");
+    //         isValid = false;
+    //     }
 
-        // Validar email con regex
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            alert("Por favor, ingresa un email válido.");
-            isValid = false;
-        }
+    //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //     if (!emailRegex.test(email)) {
+    //         alert("Por favor, ingresa un email válido.");
+    //         isValid = false;
+    //     }
 
-        // Validar tipo de evento
-        if (eventType === '...') {
-            alert("Por favor, selecciona un tipo de evento.");
-            isValid = false;
-        }
+    //     if (eventType === '...') {
+    //         alert("Por favor, selecciona un tipo de evento.");
+    //         isValid = false;
+    //     }
 
-        // Validar descripción
-        if (description === '') {
-            alert("Por favor, proporciona una breve descripción del evento.");
-            isValid = false;
-        }
+    //     if (description === '') {
+    //         alert("Por favor, proporciona una breve descripción del evento.");
+    //         isValid = false;
+    //     }
 
-        // Si el formulario es válido, enviar
-        if (isValid) {
-            alert("Formulario enviado correctamente.");
-            this.submit(); // Envía el formulario si todo está correcto
-        }
-    });
+    //     if (isValid) {
+    //         alert("Formulario enviado correctamente.");
+    //         this.submit();
+    //     }
+    // });
 });
 
 // Media query que cambia el grid del html de quienes somos 
