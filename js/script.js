445c5cc3d7742608e24264d1fcc63f802c674e47
xiangsoto <xiangsoto.145@gmail.com>
2025-01-10 01:06:59 +0100

    corrections, agregué el main

--- js/script.js
+++ js/script.js
@@ -6,154 +6,61 @@ $(document).ready(function() {
     });
 
     // QUITAR CLASES BORDER TOP Y BORDER BOTTOM
-    $(document).ready(function() {
-        function updateBorders() {
-          if ($(window).width() >= 767) {
-            $("nav .nav-item").removeClass("border-top border-bottom");
-          } else {
-            $("nav .nav-item").addClass("border-top");
-            $("nav .nav-item:last").addClass("border-bottom");
-          }
+    function updateBorders() {
+        if ($(window).width() >= 767) {
+        $("nav .nav-item").removeClass("border-top border-bottom");
+        } else {
+        $("nav .nav-item").addClass("border-top");
+        $("nav .nav-item:last").addClass("border-bottom");
         }
-        
-        updateBorders();
+    }
     
-        $(window).resize(function() {
-          updateBorders();
-        });
+    updateBorders();
+
+    $(window).resize(function() {
+        updateBorders();
     });
 
     // CARRUSEL CON SWIPER
-    $(document).ready(function () {
-        var swiper = new Swiper('.swiper', {
-            loop: true,
-            autoplay: {
-                delay: 5000,
-                disableOnInteraction: false,
-            },
-            mousewheel: true,
-            centeredSlides: true,
-            slidesPerView: 'auto',
-            spaceBetween: 0,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            on: {
-                slideChangeTransitionStart: function () {
-                    $('.swiper-slide').removeClass('swiper-slide-active');
-                    var activeIndex = swiper.activeIndex;
-                    var activeSlide = swiper.slides[activeIndex];
-                    $(activeSlide).addClass('swiper-slide-active');
-                }
+    var swiper = new Swiper('.swiper', {
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        mousewheel: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        spaceBetween: 0,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.swiper-slide').removeClass('swiper-slide-active');
+                var activeIndex = swiper.activeIndex;
+                var activeSlide = swiper.slides[activeIndex];
+                $(activeSlide).addClass('swiper-slide-active');
             }
-        });
+        }
     });
 
     // VALIDAR FORM
-    // $('form').submit(function (event) {
-    //     event.preventDefault();
+    $(".formularioCatering").on("submit", function (e) {
+        e.preventDefault();
 
-    //     let isValid = true;
-    //     const name = $('#exampleFormControlInput1[type="text"]').val().trim();
-    //     const email = $('#exampleFormControlInput1[type="email"]').val().trim();
-    //     const eventType = $('#exampleFormControlSelect1').val();
-    //     const description = $('#exampleFormControlTextarea1').val().trim();
+        var form = $("#cateringValidation")[0];
+        var isValid = form.checkValidity();
 
-    //     if (name === '') {
-    //         alert("Por favor, ingresa tu nombre y apellidos.");
-    //         isValid = false;
-    //     }
+        if (isValid) {
+            $('#reservationModal').modal('show');
+        } else {
+            form.classList.add('was-validated');
+        }
+    });
 
-    //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    //     if (!emailRegex.test(email)) {
-    //         alert("Por favor, ingresa un email válido.");
-    //         isValid = false;
-    //     }
-
-    //     if (eventType === '...') {
-    //         alert("Por favor, selecciona un tipo de evento.");
-    //         isValid = false;
-    //     }
-
-    //     if (description === '') {
-    //         alert("Por favor, proporciona una breve descripción del evento.");
-    //         isValid = false;
-    //     }
-
-    //     if (isValid) {
-    //         alert("Formulario enviado correctamente.");
-    //         this.submit();
-    //     }
-    // });
-});
-
-// Media query que cambia el grid del html de quienes somos 
-const mediaQuery = window.matchMedia("(max-width: 767px)");
-
-if (window.location.pathname.includes("quienes-somos.html")) {
-function handleMobileView(e) {
-    if (e.matches) {
-        document.getElementById("patatas-intro").innerHTML = `
-               <div class="container-fluid">
-                <div class="row">
-                    <div class="col">
-                        <img src="/media/img/patatas-3.webp" class="img-fluid" alt="Patatas fritas">
-                        <p>
-                            Las patatas
-                        </p>
-                        <img src="/media/img/patatas-2.jpg" class="img-fluid" alt="Patatas fritas">
-                        <p>
-                            que te harán
-                        </p>
-                        <img src="/media/img/patatas-1-1.avif" class="img-fluid" alt="Patatas fritas">
-                        <p>
-                            chuparte los dedos
-                        </p>
-                    </div>
-                </div>
-            </div>
-        `;
-    } else {
-        // Cambios para pantallas más grandes
-        document.getElementById("patatas-intro").innerHTML = `
-           <div class="container-fluid">
-                <div class="row">
-                    <div class="col">
-                        <img src="media/img/patatas-3.webp" class="img-fluid" alt="Patatas fritas">
-                    </div>
-                    <div class="col d-flex align-items-center justify-content-center">
-                        <p>Las patatas</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col d-flex align-items-center justify-content-center">
-                        <p>que te harán</p>
-                    </div>
-                    <div class="col">
-                        <img src="media/img/patatas-2.jpg" class="img-fluid" alt="Patatas fritas">
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <img src="media/img/patatas-1-1.avif" class="img-fluid" alt="Patatas fritas">
-                        </div>
-                        <div class="col d-flex align-items-center justify-content-center">
-                            <p>chuparte los dedos</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-    }
-}
-
-mediaQuery.addEventListener("change", handleMobileView);
-
-handleMobileView(mediaQuery);
-}
-
-// Cerrar menu al clicar icono hamburguesa
-$(document).ready(function () {
+    // CERRAR MENÚ - IRENE
     const $navbarToggler = $(".navbar-toggler");
     const $navbarCollapse = $("#navbarNav");
 
@@ -166,4 +73,68 @@ $(document).ready(function () {
             $navbarCollapse.removeClass("show");
         }
     });
+
+    // Media query que cambia el grid del html de quienes somos 
+    const mediaQuery = window.matchMedia("(max-width: 767px)");
+
+    if (window.location.pathname.includes("quienes-somos.html")) {
+    function handleMobileView(e) {
+        if (e.matches) {
+            document.getElementById("patatas-intro").innerHTML = `
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <img src="/media/img/patatas-3.webp" class="img-fluid" alt="Patatas fritas">
+                            <p>
+                                Las patatas
+                            </p>
+                            <img src="/media/img/patatas-2.jpg" class="img-fluid" alt="Patatas fritas">
+                            <p>
+                                que te harán
+                            </p>
+                            <img src="/media/img/patatas-1-1.avif" class="img-fluid" alt="Patatas fritas">
+                            <p>
+                                chuparte los dedos
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        } else {
+            // Cambios para pantallas más grandes
+            document.getElementById("patatas-intro").innerHTML = `
+            <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <img src="media/img/patatas-3.webp" class="img-fluid" alt="Patatas fritas">
+                        </div>
+                        <div class="col d-flex align-items-center justify-content-center">
+                            <p>Las patatas</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col d-flex align-items-center justify-content-center">
+                            <p>que te harán</p>
+                        </div>
+                        <div class="col">
+                            <img src="media/img/patatas-2.jpg" class="img-fluid" alt="Patatas fritas">
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <img src="media/img/patatas-1-1.avif" class="img-fluid" alt="Patatas fritas">
+                            </div>
+                            <div class="col d-flex align-items-center justify-content-center">
+                                <p>chuparte los dedos</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    }
+
+    mediaQuery.addEventListener("change", handleMobileView);
+
+    handleMobileView(mediaQuery);
+    }
 });
